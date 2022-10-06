daff8dd13622029fc7e4988cb02390083482baeb
Opiskull <opi@peerzone.net>
2022-10-06 22:52:31 +0200

    Add code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+const encodeMap = {
+  Ä: "\\u00c4",
+  ä: "\\u00e4",
+  Ö: "\\u00d6",
+  ö: "\\u00f6",
+  Ü: "\\u00dc",
+  ü: "\\u00fc",
+  ß: "\\u00df",
+};
+const encodeKeys = Object.keys(encodeMap);
+
+const replaceValues = (value) => {
+  let encodedValue = value;
+  encodeKeys.forEach((k) => {
+    encodedValue = encodedValue.replaceAll(k, encodeMap[k]);
+  });
+  return encodedValue;
+};
 
 function App() {
+  const [value, setValue] = useState("");
+  const [encodedValue, setEncodedValue] = useState("");
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    setEncodedValue(replaceValues(e.target.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <div>
+        <h1>input</h1>
+        <div>
+          <textarea
+            style={{ width: "100%", height: "350px", resize: "none" }}
+            value={value}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+      </div>
+      <div>
+        <h1>output</h1>
+        <div>
+          <textarea
+            style={{ width: "100%", height: "350px", resize: "none" }}
+            readOnly={true}
+            value={encodedValue}
+          ></textarea>
+        </div>
+      </div>
     </div>
   );
 }
